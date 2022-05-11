0fa796f98e2c0fdecb7478e44004086fb80276ba
Alex <olexandrpasalsky@gmail.com>
2022-05-11 15:04:23 +0300

    add Classes lab1 Storage

---
+++ Lab2/Lab2/Storage/Producer.cs
@@ -0,0 +1,13 @@
+﻿namespace Lab1
+{
+    class Producer
+    {
+        public string Name { get; set; }       // ім'я виробника
+        public string Country { get; set; }    //країна розташування виробника
+
+        public override string ToString()
+        {
+            return string.Format(@"{0} {1}",  Name, Country);
+        }
+    }
+}
---
+++ Lab2/Lab2/Storage/Product.cs
@@ -0,0 +1,23 @@
+﻿namespace Lab1
+{
+    class Product
+    {
+        public string Code { get; set; }         // код товару
+        public double Price { get; set; }        // ціна за одиницю
+        public int Number { get; set; }          // кількість в пачці
+        public string DateArrival { get; set; }  // дата прибуття на склад
+        public double Weight { get; set; }        // вага
+
+        public override string ToString()
+        {
+            return string.Format(@"
+                Товар: 
+                    Код: {0} 
+                    Ціна одиниці: {1} грн
+                    Кількість: {2} шт
+                    Дата привезення: {3}
+                    Вага одиниці: {4} кг", Code, Price, Number, DateArrival, Weight);
+        }
+    }
+}
+
