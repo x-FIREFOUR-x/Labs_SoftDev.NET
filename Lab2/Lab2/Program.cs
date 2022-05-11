024769e383692a17579c27e7f1fa4346ed55400b
Alex <olexandrpasalsky@gmail.com>
2022-05-11 15:02:08 +0300

    test create xml

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace Lab2
 {
@@ -6,7 +11,55 @@ namespace Lab2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = Encoding.UTF8;
+
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent = true;
+
+            IList<User> users = new List<User> {
+                new User ("Bill Gates", "Microsoft", 48),
+                new User ("Larry Page", "Google", 42)
+            };
+
+
+            using (XmlWriter writer = XmlWriter.Create("users.xml", settings))
+            {
+                writer.WriteStartElement("users");
+                foreach (User user in users)
+                {
+                    writer.WriteStartElement("user");
+                    writer.WriteElementString("name", user.Name);
+                    writer.WriteElementString("company", user.Company);
+                    writer.WriteElementString("age", user.Age.ToString());
+                    writer.WriteEndElement();
+                }
+                writer.WriteEndElement();
+            }
+
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load("users.xml");
+
+        }
+
+        class User
+        {
+            public string Name { get; set; }
+            public string Company { get; set; }
+            public int Age { get; set; }
+            public User()
+            {
+            }
+            /// <param name="name">Им'я</param>
+            /// <param name="company">Компанія</param>
+            /// <param name="age">Вік</param>
+            public User(string name, string company, int age)
+            {
+                Name = name;
+                Company = company;
+                Age = age;
+            }
         }
     }
+
 }
