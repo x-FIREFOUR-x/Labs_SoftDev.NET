2115e5675a175508d4d865f9e98eba273eb26763
Alex <olexandrpasalsky@gmail.com>
2022-05-22 02:02:47 +0300

    clean code (finish)

--- Lab4/Lab4/Decorator/Character.cs
+++ Lab4/Lab4/Decorator/Character.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Lab4
+﻿namespace Lab4
 {
     public interface Character
     {
--- Lab4/Lab4/Program.cs
+++ Lab4/Lab4/Program.cs
@@ -6,13 +6,20 @@ namespace Lab4
     {
         static void Main(string[] args)
         {
-            Character person = new NumberCharacter(10, 10, 10);
+            Character person = new NumberCharacter(177, 80.8, 19);
             person = new Statura(person, EnumStatura.Mesomorphic);
             person = new Sex(person, EnumSex.Man);
             person = new Activity(person, EnumActivity.Hight);
             Console.WriteLine(person.Description());
             Console.WriteLine($"Kkal used day: {person.CalculateCalories()}");
 
+
+            Character person2 = new NumberCharacter(55, 167, 19);
+            person2 = new Statura(person2, EnumStatura.Dolichomorphic);
+            person2 = new Sex(person2, EnumSex.Man);
+            person2 = new Activity(person2, EnumActivity.Midle);
+            Console.WriteLine(person2.Description());
+            Console.WriteLine($"Kkal used day: {person2.CalculateCalories()}");
         }
     }
 }
